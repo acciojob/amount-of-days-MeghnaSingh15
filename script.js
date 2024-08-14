f8a994a1d834b12a5c5a52a588214e4bd745bd80
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-08-14 11:25:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function daysOfAYear() {
+	if ((year % 4 == 0) && (year % 100 != 0) ||(year % 400 ==0)){
+		console.log("365");
+		}
+	else {
+	    console.log("366");
+	}
+}
+year = prompt("Please enter a year:");
+daysOfAYear(year);
